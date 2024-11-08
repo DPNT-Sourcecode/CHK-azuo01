16db94247d1c749572c6c155b509f332822ae228
sbx_user1051 <sbx_user1051@169.254.39.21>
2024-11-08 22:39:25 +0000

    Fri Nov 08 22:39:25 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,15 +4,16 @@
 # skus = unicode string
 def checkout(skus):
 
-    a, b, c, d = 50, 30, 20, 15
+    a, b, c, d, e = 50, 30, 20, 15, 40
 
     a_count = skus.count('A')
     b_count = skus.count('B')
     c_count = skus.count('C')
     d_count = skus.count('D')
+    e_count = skus.count('E')
 
     for i in list(skus):
-        if i not in ['A', 'B', 'C', 'D']:
+        if i not in ['A', 'B', 'C', 'D', 'E']:
             return -1
         
     if a_count >= 3:
@@ -31,5 +32,6 @@ def checkout(skus):
         return a_sum + b_sum + (c * c_count) + (d * d_count)
     else:
         return 0
+
 
 
