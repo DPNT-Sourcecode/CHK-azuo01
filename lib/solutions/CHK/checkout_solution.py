f295e48b99b8a8b604def44bf7f4207d0f18fe9c
sbx_user1051 <sbx_user1051@169.254.39.21>
2024-11-08 23:44:40 +0000

    Fri Nov 08 23:44:40 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -41,8 +41,8 @@ def checkout(skus):
         q, mod = e_count // 2, e_count % 2
         if b_count % 2 == 0 and q <= b_count:
             b_sum -= b_count // 2 * 45
-        elif b_count >= 1 and q <= b_count:
-            b_sum -= q * 30
+        # elif b_count >= 1 and q <= b_count:
+        #     b_sum -= q * 30
         else:
             b_sum = 0
 
@@ -50,7 +50,3 @@ def checkout(skus):
         return a_sum + b_sum + (c * c_count) + (d * d_count) + (e * e_count)
     else:
         return 0
-
-
-
-
